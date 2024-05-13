4cf9860f055a7d288e845e3165a719d64dfe6b61
Matthew Krische <krisch1@stolaf.edu>
2024-05-12 20:22:10 -0500

    more quiz updates

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function ProfessionalIdea() {
 function goToQuiz() {
   location.href = "quiz.html"
 }
+
+function replaceButtonX() {
+  var image = document.createElement("img");
+  image.source = "X.png"; 
+  image.alt = "Wrong!"; 
+  document.getElementById("wrong").replaceWith(image);
+}
+
+function replaceButtonCheck() {
+  var image = document.createElement("img");
+  image.source = "check.png"; 
+  image.alt = "Wrong!"; 
+  document.getElementById("correct").replaceWith(image);
+}
+
+document.getElementById("correct").addEventListener("click", replaceButtonCheck);
+
+document.getElementById("wrong").addEventListener("click", replaceButtonX);
